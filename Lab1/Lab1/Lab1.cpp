72021ead9d906e599a44ae3c33af86ebe82b66b2
KursovIvan <kursov24@yandex.ru>
2021-09-12 18:31:43 +0500

    Сумма, разность, ср. арифметическое

---
+++ Lab1/Lab1/Lab1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int a;
+    int b;
+    
+    cout << "Введите два числа: ";
+    cin >> a >> b;
+
+    cout << "Сумма: " << a + b << endl << "Разность: " << a - b << endl << "Среднее арифметическое модулей: " << (abs(a) + abs(b)) / 2 << endl;
+
+    _getch();
+}
